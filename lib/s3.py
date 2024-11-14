edfdbd1765c4effa8e22932af0e7699945e40130
Maxwell Austensen <m.austensen@gmail.com>
2024-11-14 12:38:14 -0500

    specify s3 server side encryption type (SSE-S3)

--- lib/s3.py
+++ lib/s3.py
@@ -55,7 +55,8 @@ def put_object(s3_client, dest_bucket_name, dest_object_name, src_data, content_
 		'Bucket': dest_bucket_name, 
 		'Key': dest_object_name, 
 		'Body': object_data,
-		'ContentType': content_type
+		'ContentType': content_type,
+		'ServerSideEncryption': 'AES256' # Amazon S3 managed keys (SSE-S3)
 	}
 
 	if cache_control != '':
